e35170e0a70209e8b27c2f2e862f00813e527608
Ab03009 <Albertmanukyan678@gmail.com>
2026-04-13 18:00:10 +0400

    Switch to new repo and fix styles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Оставляем base пустым или './', чтобы стили искались в текущей папке
   base: './', 
 })
