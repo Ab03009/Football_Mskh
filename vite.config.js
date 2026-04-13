5b0ff16d9e6aa71a54e8b916d27d26f2282fefa1
Ab03009 <Albertmanukyan678@gmail.com>
2026-04-13 18:02:32 +0400

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Оставляем base пустым или './', чтобы стили искались в текущей папке
-  base: './', 
+  base: '/Football_Mskh/', // Обязательно: название репозитория слэшами
 })
